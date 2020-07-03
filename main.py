2a31711100b33a2e92cf4fa178f74394ffe78203
Adam Romayor <adamromayor@gmail.com>
2020-07-03 16:25:25 -0700

    Initial Commit

---
+++ main.py
@@ -0,0 +1,93 @@
+#!/usr/bin/env python
+
+import sys
+import datetime
+import pprint
+import twitter
+import requests, json
+import math
+from tokens import *
+
+sys.path.insert(1, '/python-twitter')
+api = twitter.Api(consumer_key=consumerAPI.apiKey,
+                  consumer_secret=consumerAPI.apiSecret,
+                  access_token_key=accessToken.accessToken,
+                  access_token_secret=accessToken.accessTokenSecret)
+
+user = 'Perf_Prac'
+donaldTrump = 'realDonaldTrump'
+pp = pprint.PrettyPrinter(indent=1)
+
+# Prints weather for the week
+# Returns weather for today
+
+
+def get_weather():
+    base_url = "http://api.openweathermap.org/data/2.5/onecall?"
+    lat = "37.3382"
+    lon = "-121.8863"
+    city_name = "San Jose" # corresponds to the lat and lon
+    complete_url = base_url + "lat=" + lat + "&lon=" + lon + "&units=imperial&appid=" + weatherKey.key
+    response = requests.get(complete_url)
+    x = response.json()
+
+    # Get today's day and month
+    today = datetime.date.today()
+    today_month = today.month
+    today_day = today.day
+    today_weather = ""
+
+    print("Today is ", today_month, " ", today_day)
+    for day in x['daily']:
+        date = day['dt']
+        day_of_week = datetime.datetime.utcfromtimestamp(date)
+        day_format = day_of_week.strftime('%b %d, %Y')
+
+        temperature = str(math.floor(day['temp']['max']))
+        description = str(day['weather'][0]['description'])
+        if "clear" in description:
+            description = "Clear skies! Go outside and enjoy the sun."
+        elif "clouds" in description:
+            description = "Cloudy today..."
+        elif "rain" in description:
+            description = "It's going to rain! Bring an umbrella!!"
+        elif "drizzle" in description:
+            description = "It's going to drizzle! Maybe bring an umbrella?"
+        elif "thunder" in description:
+            description = "Thunderstorms! Stay inside or bring an umbrella!"
+
+        if today_day == day_of_week.day and today_month == day_of_week.month:
+
+            today_weather = "Today in " + city_name + " it's " + temperature + "°F. " + description
+
+        nice = day_format + " " + temperature + "°F. " + description
+        print(nice)
+
+    return today_weather
+
+
+def tweet_weather_for_today():
+    today_weather = get_weather()
+    print(today_weather)
+    api.PostUpdate(today_weather)
+
+
+def main():
+    """
+    #print(api.VerifyCredentials())
+    statuses = api.GetUserTimeline(screen_name=donaldTrump)
+    trends = api.GetTrendsCurrent()
+
+    limit = 10
+    for index, trend in zip(range(limit),trends):
+        pp.pprint(trend.name)
+
+    for index, status in zip(range(limit), statuses):
+        pp.pprint(status.location)
+    """
+    print(get_weather())
+    return
+
+
+if __name__ == "__main__":
+    main()
